be1885b93afa5fb9641057fc064a884113811537
harshit gupta <harshit.28.sps@gmail.com>
2021-07-18 18:11:13 +0530

    Dark Theam, Responsive & facebook-twitter sharing.

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 let isAdmin = false;
 
-const homeStartingContent = "Hope you all are doing great, This is my personal blogging website for keeping handy info and share knowledge for various projects I have done so far. In the future, I will develop this application more so that everyone can make their own account and share their project and knowledge with us.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// const homeStartingContent = "Hope you all are doing great, This is my personal blogging website for keeping handy info and share knowledge for various projects I have done so far. In the future, I will develop this application more so that everyone can make their own account and share their project and knowledge with us.";
+// const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+// const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -35,6 +35,10 @@ const blogsSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  description: {
+    type: String,
+    required: true
+  },
   body: {
     type: String,
     required: true
@@ -48,6 +52,13 @@ const blogsSchema = new mongoose.Schema({
     required: true
   },
   category: {
+    type: String,
+    required: true
+  },
+  youtube:{
+    type: String
+  },
+  publishedDate: {
     type: String,
     required: true
   }
@@ -86,6 +97,12 @@ const Admin = new mongoose.model('Admin', adminSchema);
 //     admin.save();
 // });
 
+let options = { year: 'numeric', month: 'long', day: 'numeric' };
+let today  = new Date();
+
+let todaysDate = today.toLocaleDateString("en-US", options);
+console.log(todaysDate);
+
 app.get("/", function(req, res) {
   Blogs.find({}, null, {
     sort: {
@@ -93,8 +110,8 @@ app.get("/", function(req, res) {
     }
   }, function(err, foundData) {
     if (!err) {
+      // console.log(foundData);
       res.render("home", {
-        home: homeStartingContent,
         posts: foundData
       });
     }
@@ -107,11 +124,15 @@ app.get("/posts/:post", function(req, res) {
       foundData.forEach(function(post) {
 
         if (_.lowerCase(post._id) === _.lowerCase(req.params.post)) {
+          console.log(post._id, post.description);
           res.render("post", {
+            postId: post._id,
             postTitle: post.title,
             postBody: post.body,
             postImage: post.image,
-            products: post.monetization
+            products: post.monetization,
+            projectVid: post.youtube,
+            description: post.description
           });
         }
       })
@@ -136,7 +157,7 @@ app.post("/login", function(req, res) {
     if (name_login === name_db && email_login === email_db) {
       bcrypt.compare(password_login, password_db, function(err, result) {
         if (result === true) {
-          console.log("result");
+          // console.log("result");
           isAdmin = true;
           res.redirect("/compose");
         } else {
@@ -152,6 +173,7 @@ app.post("/login", function(req, res) {
 
 app.get("/compose", function(req, res) {
   if (isAdmin) {
+    isAdmin = false
     res.render("compose");
   } else {
     res.render("login");
@@ -161,9 +183,12 @@ app.get("/compose", function(req, res) {
 app.post("/compose", function(req, res) {
   const post = new Blogs({
     title: req.body.postTitle,
+    description: req.body.description,
     body: req.body.postBody,
     image: req.body.postImage,
     category: req.body.selector,
+    youtube: req.body.project,
+    publishedDate: todaysDate,
     monetization: req.body.products
   });
   isAdmin = false;
